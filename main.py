1e9246c262f49511a283a8cfaa0fc33f2abe90ea
ecaromeo <89895671+ecaromeo@users.noreply.github.com>
2021-09-01 16:40:20 +0200

    added stupidpolicy

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ Author: Romeo Casesa
 import gym
 import time
 
+
+def stupidpolicy(observation):
+    cpos, cvel, pang, pvel = observation
+    action = 1 if pvel > 0 else 0
+    return action
+
+
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
-    env.reset()
+    observation = env.reset()
     for _ in range(1000):
         env.render()
-        action = env.action_space.sample()     # take a random action
+        # action = env.action_space.sample()     # take a random action
+        action = stupidpolicy(observation)
         observation, reward, done, info = env.step(action)
         if done:
             print("[msg] >> Episode terminated.")
