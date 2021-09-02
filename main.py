3c85b8dae8d41252d7c0c32978cddf57eacef2ce
ecaromeo <89895671+ecaromeo@users.noreply.github.com>
2021-09-02 14:20:31 +0200

    Update policy

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def stupidpolicy(observation):
 
 def apprx_costFunction(observation):
     cpos, cvel, pang, pvel = observation
-    cost = np.abs(pang) * np.abs(cvel) + np.abs(cpos)* np.abs(pvel)
+    cost = 1/(0.418 - np.abs(pang)) + 1/(4.8 - np.abs(cpos))
     return cost
 
 
@@ -26,18 +26,19 @@ def calc_CostToGo(observation, iterations):
     Recursively calculate the cost to go
     """
     action_space = np.array([0, 1])
-    new_states = [calcNewState(observation,
-                               action) for action in action_space]
+    new_states = [apprx_calcNewState(observation,
+                                     action) for action in action_space]
     immediate_costs = [apprx_costFunction(state) for state in new_states]
 
     if iterations == 0:
         return np.min(immediate_costs)
     else:
-        return np.min([immed_cost + calc_CostToGo(newstate, iterations - 1) for immed_cost,
-                       newstate in zip(immediate_costs, new_states)])
+        return np.min([immed_cost + calc_CostToGo(newstate, iterations - 1)
+                       for immed_cost, newstate in 
+                       zip(immediate_costs, new_states)])
 
 
-def calcNewState(observation, action):
+def apprx_calcNewState(observation, action):
     """
     Calculate new state, given the current state
     and the action.
@@ -46,10 +47,14 @@ def calcNewState(observation, action):
     @param action Takes value in the set [0, 1]
     """
     cpos, cvel, pang, pvel = observation
-    new_cpos = cpos + cvel
-    new_cvel = cvel + 0.01*(2 * action - 1)
-    new_pang = pang + pvel
-    new_pvel = pvel + 0.01*(- 2 * action + 1)
+    new_cpos = cpos + 0.1*cvel
+    if np.abs(new_cpos) > 4.8:
+        new_cpos = np.sign(new_cpos)*4.8
+    new_cvel = cvel + 0.1*(2 * action - 1)
+    new_pang = pang + 0.1*pvel
+    if np.abs(new_pang) > 0.418:
+        new_pang = np.sign(new_pang)*0.418
+    new_pvel = pvel + 0.1*(- 2 * action + 1)
     new_observation = np.array([new_cpos, new_cvel,
                                 new_pang, new_pvel])
     return new_observation
@@ -60,8 +65,8 @@ def plcy_1(observation):
     Policy 1.
     """
     action_space = np.array([0, 1])
-    new_states = [calcNewState(observation,
-                               action) for action in action_space]
+    new_states = [apprx_calcNewState(observation,
+                                     action) for action in action_space]
     immediate_cost = [apprx_costFunction(state) for state in new_states]
     cost_to_go = [calc_CostToGo(state, 5) for state in new_states]
     slctd = np.argmin(np.add(immediate_cost, cost_to_go))
@@ -72,14 +77,16 @@ def plcy_1(observation):
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
     observation = env.reset()
-    for _ in range(1000):
+    tot_reward = 0
+    while True:
         env.render()
         # action = env.action_space.sample()     # take a random action
         # action = stupidpolicy(observation)
         action = plcy_1(observation)
         observation, reward, done, info = env.step(action)
+        tot_reward += reward
         if done:
-            print("[msg] >> Episode terminated.")
+            print("[msg] >> Episode terminated with score:", tot_reward)
             time.sleep(2)
             break
     env.close()
